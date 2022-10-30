4d488f538714096611486a66b910de96ab04efa3
naocraft0111 <naoki1306@gmail.com>
2022-10-30 17:48:32 +0900

    スワイプページの実装

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,10 +14,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::controller(UserController::class)->middleware(['auth'])
+->group(function() {
+    // スワイプページ
+    Route::get('/users', 'index')->name('users.index');
+});

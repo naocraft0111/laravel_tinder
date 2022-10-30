985522e097534a8280b55ee742449bba034321d7
naocraft0111 <naoki1306@gmail.com>
2022-10-30 21:26:52 +0900

    img_urlがnullでもサイトがエラーが出ないように変更

--- resources/views/pages/user/index.blade.php
+++ resources/views/pages/user/index.blade.php
@@ -3,10 +3,14 @@
 @section('content')
     <div class="p-user-index">
         <div class="tphoto">
+            @if (isset($user->img_url))
             <img src="{{ $user->img_url }}"
                 title="tphoto"
                 alt="Tinder Photo" />
-            <div class="tname">{{ $user->name }}</div>
+                <div class="tname">{{ $user->name }}</div>
+            @else
+            <div class="text-center">現在マッチング対象の人物がヒットしません</div>
+            @endif
         </div>
 
         <div class="tcontrols">
@@ -17,14 +21,17 @@
                             method="POST">
                             @csrf
 
-                            <input type="hidden" name="to_user_id" value="{{ $user->id }}">
-                            <input type="hidden" name="is_like" value="0">
+                            @if (isset($user->id))
+                                <input type="hidden" name="to_user_id" value="{{ $user->id }}">
+                                <input type="hidden" name="is_like" value="0">
+                                <button class="tno"
+                                    type="submit">
+                                    <i class="fa fa-times"
+                                        aria-hidden="true"></i>
+                                </button>
+                            @else
+                            @endif
 
-                            <button class="tno"
-                                type="submit">
-                                <i class="fa fa-times"
-                                    aria-hidden="true"></i>
-                            </button>
                         </form>
                     </div>
                     <div class="col-md-6 mb-1">
@@ -32,14 +39,17 @@
                             method="POST">
                             @csrf
 
+                            @if (isset($user->id))
                             <input type="hidden" name="to_user_id" value="{{ $user->id }}">
                             <input type="hidden" name="is_like" value="1">
-
                             <button class="tyes"
                                 type="submit">
                                 <i class="fa fa-heart"
                                     aria-hidden="true"></i>
                             </button>
+                            @else
+                            @endif
+
                         </form>
                     </div>
                 </div>
